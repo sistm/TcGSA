11d22756ac414d9ca0111a89d788082a90c8b38f
Boris Hejblum <boris.hejblum@isped.u-bordeaux2.com>
2013-08-07 13:54:30 +0200

    Example data improved

--- R/TcGSA-package.R
+++ R/TcGSA-package.R
@@ -6,8 +6,8 @@
 #'\tabular{ll}{
 #'Package: \tab TcGSA\cr
 #'Type: \tab Package\cr
-#'Version: \tab 0.9.46\cr
-#'Date: \tab 2013-08-06\cr
+#'Version: \tab 0.9.47\cr
+#'Date: \tab 2013-08-07\cr
 #'License:\tab \href{http://www.gnu.org/licenses/lgpl.txt}{LGPL-3}\cr
 #'}
 #'The main function in this package is \code{\link{TcGSA.LR}} which performs Time-course Gene Set Analysis, and provide nice representations of its results (see \code{\link{plot.TcGSA}} and \code{\link{plot1GS}}).
