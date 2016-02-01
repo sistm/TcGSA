f9309d3db695ba17987e0a6dc644cc2cf6b9eaf2
borishejblum <boris.hejblum@isped.u-bordeaux2.fr>
2016-02-01 11:50:54 -0500

    CITATION typo

--- R/TcGSA-package.R
+++ R/TcGSA-package.R
@@ -6,8 +6,8 @@
 #'\tabular{ll}{
 #'Package: \tab TcGSA\cr
 #'Type: \tab Package\cr
-#'Version: \tab 0.10.1\cr
-#'Date: \tab 2015-12-22\cr
+#'Version: \tab 0.10.2\cr
+#'Date: \tab 2016-02-01\cr
 #'License:\tab \href{http://www.gnu.org/licenses/lgpl.txt}{LGPL-3}\cr
 #'}
 #'The main function in this package is \code{\link{TcGSA.LR}} which performs Time-course Gene Set Analysis, and provide nice representations of its results (see \code{\link{plot.TcGSA}} and \code{\link{plot1GS}}).
